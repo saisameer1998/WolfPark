4b0e9fbe1881a1f3fd830e133e88aa14fc992e09
Chinzzii <chinmay.singhania@gmail.com>
2023-11-05 14:05:24 -0500

    Added Basic Project Structure

---
+++ src/classes/Vehicles.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class Vehicles extends WolfPark {
+    void GetVehicleInfo() {
+    }
+
+    void AddVehicle() {
+    }
+
+    void DeleteVehicle() {
+    }
+
+    void UpdateVehicle() {
+    }
+
+}
