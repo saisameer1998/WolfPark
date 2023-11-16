870d4e6242e0f2b2dadcfacdca2f005327875fb2
Chinzzii <chinmay.singhania@gmail.com>
2023-11-16 16:29:30 -0500

    Fixed Zone Module

--- src/classes/Zone.java
+++ src/classes/Zone.java
@@ -6,70 +6,101 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class Zone extends WolfPark{
-	
-	String zoneId;
+public class Zone extends WolfPark {
 
-    public Zone(String zoneId) {
-        this.zoneId = zoneId;
-    }
-    
-    
-    public void enterZoneInfo() {
-        try (Connection connection = DriverManager.getConnection("jdbc:your_database_url", "username", "password")) {
-            String sql = "INSERT INTO zone_assignments  VALUES (?)";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, zoneId);
-                preparedStatement.executeUpdate();
-            }
+    static String jdbcURL = "jdbc:mariadb://classdb2.csc.ncsu.edu:330/";
+    static String user = "sguttha";
+    static String pswd = "Maria@MegaMind1";
+
+    // String zoneId;
+
+    // public Zone(String zoneId) {
+    // this.zoneId = zoneId;
+    // }
+
+    public void enterZoneInfo(String zoneId) throws SQLException {
+        Connection connection = connectToDatabase(jdbcURL, user, pswd);
+        try {
+            String sql = "INSERT INTO zone_assignments VALUES (?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, zoneId);
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            close(connection);
         }
     }
-    
-    
-    public void updateZoneInfo(String newZoneId) {
-        try (Connection connection = DriverManager.getConnection("jdbc:your_database_url", "username", "password")) {
+
+    public void updateZoneInfo(String zoneId, String newZoneId) throws SQLException {
+        Connection connection = connectToDatabase(jdbcURL, user, pswd);
+        try {
             String sql = "UPDATE zones SET zone_id = ? WHERE zone_id = ?";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, newZoneId);
-                preparedStatement.setString(2, zoneId);
-                preparedStatement.executeUpdate();
-            }
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, newZoneId);
+            preparedStatement.setString(2, zoneId);
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            close(connection);
         }
     }
-    
-    
-    public void deleteZoneInfo() {
-        try (Connection connection = DriverManager.getConnection("jdbc:your_database_url", "username", "password")) {
+
+    public void deleteZoneInfo(String zoneId) throws SQLException {
+        Connection connection = connectToDatabase(jdbcURL, user, pswd);
+        try {
             String sql = "DELETE FROM zones WHERE zone_id = ?";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, zoneId);
-                preparedStatement.executeUpdate();
-            }
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, zoneId);
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            close(connection);
         }
     }
-    
-    public String getZoneInfo() {
+
+    public String getZoneInfo(String zoneId) throws SQLException {
         String zoneInfo = null;
-        try (Connection connection = DriverManager.getConnection("jdbc:your_database_url", "username", "password")) {
+        Connection connection = connectToDatabase(jdbcURL, user, pswd);
+        try {
             String sql = "SELECT * FROM zones WHERE zone_id = ?";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, zoneId);
-                try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                    if (resultSet.next()) {
-                        zoneInfo = resultSet.getString("zone_id");
-                    }
-                }
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, zoneId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                zoneInfo = resultSet.getString("zone_id");
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            close(connection);
         }
+
         return zoneInfo;
+    }
+
+    private static Connection connectToDatabase(String jdbcURL, String user, String pswd)
+            throws SQLException {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(jdbcURL, user, pswd);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
+    private static void close(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (Throwable whatever) {
+            }
+        }
+
     }
 
 }
