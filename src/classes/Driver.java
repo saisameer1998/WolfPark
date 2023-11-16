685f98b477ac9b9c85319211cb7c696ac4c82b47
TVT <tanishqtodkar@gmail.com>
2023-11-15 23:52:16 -0500

    updated driver.java

--- src/classes/Driver.java
+++ src/classes/Driver.java
@@ -6,20 +6,26 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Driver extends WolfPark {
-    String driverName;
-    String licenseNumber;
+    String driver_id;
+    String name;
+    String status;
+    String disability;
 
-    public Driver(String driverName, String licenseNumber) {
-        this.driverName = driverName;
-        this.licenseNumber = licenseNumber;
+    public Driver(String driver_id, String name, String status, String disability) {
+        this.driver_id = driver_id;
+        this.name = name;
+        this.status= status;
+        this.disability= disability;
     }
 
     public void enterDriverInfo() {
         try (Connection connection = DriverManager.getConnection("jdbc:your_database_url", "username", "password")) {
-            String sql = "INSERT INTO drivers VALUES (?, ?)";
+            String sql = "INSERT INTO drivers VALUES (?, ? ,? ,?)";
             try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, driverName);
-                preparedStatement.setString(2, licenseNumber);
+                preparedStatement.setString(1, driver_id);
+                preparedStatement.setString(2, name);
+                preparedStatement.setString(3, status);
+                preparedStatement.setString(4, disability);
                 preparedStatement.executeUpdate();
             } finally {
                 close(connection);
@@ -29,12 +35,12 @@ public class Driver extends WolfPark {
         }
     }
 
-    public void updateDriverInfo(String newLicenseNumber) {
+    public void updateDriverInfo(String name) {
         try (Connection connection = DriverManager.getConnection("jdbc:your_database_url", "username", "password")) {
-            String sql = "UPDATE drivers SET license_number = ? WHERE driver_name = ?";
+            String sql = "UPDATE drivers SET name = ? WHERE driver_id = ?";
             try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, newLicenseNumber);
-                preparedStatement.setString(2, driverName);
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, driver_id);
                 preparedStatement.executeUpdate();
             } finally {
                 close(connection);
@@ -46,9 +52,9 @@ public class Driver extends WolfPark {
 
     public void deleteDriverInfo() {
         try (Connection connection = DriverManager.getConnection("jdbc:your_database_url", "username", "password")) {
-            String sql = "DELETE FROM drivers WHERE driver_name = ?";
+            String sql = "DELETE FROM drivers WHERE driver_id = ?";
             try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, driverName);
+                preparedStatement.setString(1, driver_id);
                 preparedStatement.executeUpdate();
             } finally {
                 close(connection);
