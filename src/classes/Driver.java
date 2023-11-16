3222350e57f02a994e53c6ff0c36434823cf0f1c
Chinzzii <chinmay.singhania@gmail.com>
2023-11-16 17:36:18 -0500

    Fixed Driver Module

--- src/classes/Driver.java
+++ src/classes/Driver.java
@@ -1,75 +1,91 @@
 package classes;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Driver extends WolfPark {
-    String driver_id;
-    String name;
-    String status;
-    String disability;
 
-    public Driver(String driver_id, String name, String status, String disability) {
-        this.driver_id = driver_id;
-        this.name = name;
-        this.status= status;
-        this.disability= disability;
-    }
+    static String jdbcURL = "jdbc:mariadb://classdb2.csc.ncsu.edu:330/";
+    static String user = "sguttha";
+    static String pswd = "Maria@MegaMind1";
 
-    public void enterDriverInfo() {
-        try (Connection connection = DriverManager.getConnection("jdbc:your_database_url", "username", "password")) {
+    // String driver_id;
+    // String name;
+    // String status;
+    // String disability;
+
+    // public Driver(String driver_id, String name, String status, String
+    // disability) {
+    // this.driver_id = driver_id;
+    // this.name = name;
+    // this.status = status;
+    // this.disability = disability;
+    // }
+
+    public void enterDriverInfo(String driver_id, String name, String status, String disability) throws SQLException {
+        Connection connection = connectToDatabase(jdbcURL, user, pswd);
+        try {
             String sql = "INSERT INTO drivers VALUES (?, ? ,? ,?)";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, driver_id);
-                preparedStatement.setString(2, name);
-                preparedStatement.setString(3, status);
-                preparedStatement.setString(4, disability);
-                preparedStatement.executeUpdate();
-            } finally {
-                close(connection);
-            }
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, driver_id);
+            preparedStatement.setString(2, name);
+            preparedStatement.setString(3, status);
+            preparedStatement.setString(4, disability);
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            close(connection);
         }
     }
 
-    public void updateDriverInfo(String name) {
-        try (Connection connection = DriverManager.getConnection("jdbc:your_database_url", "username", "password")) {
+    public void updateDriverInfo(String name, String driver_id) throws SQLException {
+        Connection connection = connectToDatabase(jdbcURL, user, pswd);
+        try {
             String sql = "UPDATE drivers SET name = ? WHERE driver_id = ?";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, name);
-                preparedStatement.setString(2, driver_id);
-                preparedStatement.executeUpdate();
-            } finally {
-                close(connection);
-            }
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, driver_id);
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            close(connection);
         }
     }
 
-    public void deleteDriverInfo() {
-        try (Connection connection = DriverManager.getConnection("jdbc:your_database_url", "username", "password")) {
+    public void deleteDriverInfo(String driver_id) throws SQLException {
+        Connection connection = connectToDatabase(jdbcURL, user, pswd);
+        try {
             String sql = "DELETE FROM drivers WHERE driver_id = ?";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, driver_id);
-                preparedStatement.executeUpdate();
-            } finally {
-                close(connection);
-            }
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, driver_id);
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            close(connection);
         }
     }
 
-    static void close(Connection conn) {
+    private static Connection connectToDatabase(String jdbcURL, String user, String pswd)
+            throws SQLException {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(jdbcURL, user, pswd);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
+    private static void close(Connection conn) {
         if (conn != null) {
             try {
                 conn.close();
             } catch (Throwable whatever) {
             }
         }
+
     }
 }
